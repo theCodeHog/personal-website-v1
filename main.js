c063c2df146646847bfae9fb38dc82fa423fc947
Paola <paolatosca@outlook.com>
2020-07-07 13:51:32 +0200

    added galleries and structured css

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-function showGallery(galleryType) {
-  console.log(galleryType);
+//function to change CSS to switch between gallery-types
 
+function showGallery(galleryType) {
   if (galleryType !== "designer-developer-gallery") {
     document.getElementById("designer-developer-gallery").style.display =
       "none";
   }
-
   if (galleryType !== "artist-gallery") {
     document.getElementById("artist-gallery").style.display = "none";
   }
-
   if (galleryType !== "dancer-gallery") {
     document.getElementById("dancer-gallery").style.display = "none";
   }
